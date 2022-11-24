c49cc79b2e412d70fbda5ea6e8d0e7fbf21704cb
Andrey <shmalunandrey@gmail.com>
2022-11-24 16:14:33 -0500

    Implement a movie card component. Define the routing

--- src/app/user-login-form/user-login-form.component.ts
+++ src/app/user-login-form/user-login-form.component.ts
@@ -1,7 +1,8 @@
 import { Component, OnInit, Input } from '@angular/core';
 import { MatDialogRef } from '@angular/material/dialog'; // Close dialog on success
-import { UserRegistrationService } from '../fetch-api-data.service'; // API
+import { FetchApiDataService } from '../fetch-api-data.service'; // API
 import { MatSnackBar } from '@angular/material/snack-bar'; // Notifications
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-user-login-form',
@@ -12,9 +13,10 @@ export class UserLoginFormComponent implements OnInit {
   @Input() userData = { Username: '', Password: '' };
 
   constructor(
-    public fetchApiData: UserRegistrationService,
+    public fetchApiData: FetchApiDataService,
     public dialogRef: MatDialogRef<UserLoginFormComponent>,
-    public snackBar: MatSnackBar
+    public snackBar: MatSnackBar,
+    private router: Router
   ) {}
 
   ngOnInit(): void {}
@@ -30,6 +32,7 @@ export class UserLoginFormComponent implements OnInit {
         this.snackBar.open(result, 'OK', {
           duration: 2000,
         });
+        this.router.navigate(['movies']);
       },
       (result) => {
         this.snackBar.open(result, 'OK', {
