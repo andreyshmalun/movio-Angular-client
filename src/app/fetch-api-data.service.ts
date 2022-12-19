57a0bba204058217c65d4f1e9964530b9f703dec
Andrey <shmalunandrey@gmail.com>
2022-12-19 12:37:07 -0500

    replaced heroku with onrender

--- src/app/fetch-api-data.service.ts
+++ src/app/fetch-api-data.service.ts
@@ -9,7 +9,7 @@ import { Observable, throwError } from 'rxjs';
 import { flatMap, map, find } from 'rxjs/operators';
 
 //Declaring the api url that will provide data for the client app
-const apiUrl = 'https://movio-app.herokuapp.com/';
+const apiUrl = 'https://movio.onrender.com/';
 @Injectable({
   providedIn: 'root',
 })
